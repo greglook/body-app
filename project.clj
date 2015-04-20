ccd5ca39af44b9ca5436d372693a7dcfd2d4d472
Greg Look <greg@greg-look.net>
2015-04-19 20:00:44 -0700

    Add a bunch of dependencies, logback config.

--- project.clj
+++ project.clj
@@ -4,4 +4,20 @@
   :license {:name "Public Domain"
             :url "http://unlicense.org/"}
 
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies
+  [[ch.qos.logback/logback-classic "1.1.2"]
+   [clj-time "0.9.0"]
+   [compojure "1.3.1"]
+   [com.stuartsierra/component "0.2.2"]
+   [environ "1.0.0"]
+   [hiccup "1.0.5"]
+   [org.clojure/clojure "1.6.0"]
+   [org.clojure/tools.logging "0.3.1"]
+   [org.slf4j/jul-to-slf4j "1.7.6"]
+   [ring/ring-core "1.3.2"]
+   [ring/ring-jetty-adapter "1.3.2"]
+   [ring-middleware-format "0.4.0"]]
+
+  :profiles {:repl [{:source-paths ["dev"]
+                     :dependencies [[org.clojure/tools.namespace "0.2.8"]]}
+                    :local]})
