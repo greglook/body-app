27ff50e0da8ba2d580ad3ed09087292f5d7949bf
Greg Look <greg@greg-look.net>
2015-04-19 19:04:20 -0700

    Check in project skeleton.

---
+++ README.md
@@ -0,0 +1,9 @@
+Health Tracker
+==============
+
+A Clojure web-app for tracking and planning health stats.
+
+## License
+
+This is free and unencumbered software released into the public domain.
+See the UNLICENSE file for more information.
---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject mvxcvi/health-tracker "0.1.0-SNAPSHOT"
+  :description "A health tracking and planning webapp."
+  :url "https://github.com/greglook/health-tracker"
+  :license {:name "Public Domain"
+            :url "http://unlicense.org/"}
+
+  :dependencies [[org.clojure/clojure "1.6.0"]])
