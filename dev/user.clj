415af47464a8684a8f022f6affb33f05ae74d964
Greg Look <greg@greg-look.net>
2015-04-30 00:05:56 -0700

    Add datomic init and exclude dependencies.

--- dev/user.clj
+++ dev/user.clj
@@ -10,6 +10,7 @@
     [clojure.string :as str]
     [clojure.tools.namespace.repl :refer [refresh]]
     [com.stuartsierra.component :as component]
+    [datomic.api :as d]
     [mvxcvi.body.main :as main :refer [system init! start! stop!]]
     [environ.core :refer [env]]))
 
@@ -26,3 +27,19 @@
   []
   (stop!)
   (refresh :after 'user/go!))
+
+
+(defn init-database
+  "Creates and initializes a database for the given URI. If the database did
+  not exist previously, transaction data will be read and executed from the
+  given resources."
+  [uri & sources]
+  (if (d/create-database uri)
+    (let [conn (d/connect uri)]
+      (doseq [source sources]
+        (let [tx (read-string (slurp source))]
+          ; transact returns a future, which we deref to make sure any failures
+          ; are thrown as exceptions.
+          (force (d/transact conn tx))))
+      :created)
+    :existed))
