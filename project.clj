d8ce5b6e530f666d3b38f0764fe2750248145775
Greg Look <greg@greg-look.net>
2015-05-05 18:14:50 -0700

    Switch project.clj into clojurescript mode.

--- project.clj
+++ project.clj
@@ -5,26 +5,12 @@
             :url "http://unlicense.org/"}
 
   :plugins
-  [[lein-environ "1.0.0"]]
+  [[lein-cljsbuild "1.0.5"]]
 
   :dependencies
-  [[ch.qos.logback/logback-classic "1.1.3"]
-   [clj-time "0.9.0"]
-   [compojure "1.3.3"]
-   [com.cemerick/friend "0.2.1" :exclusions [org.clojure/core.cache]]
-   [com.stuartsierra/component "0.2.3"]
-   [environ "1.0.0"]
-   [hiccup "1.0.5"]
-   [metosin/ring-http-response "0.6.1"]
-   [org.clojure/clojure "1.7.0-beta1"]
-   [org.clojure/tools.logging "0.3.1"]
-   [prismatic/schema "0.4.2"]
-   [ring/ring-core "1.3.2"]
-   [ring/ring-jetty-adapter "1.3.2"]
-   [ring-middleware-format "0.5.0"]]
+  [[org.clojure/clojure "1.7.0-beta1"]
+   [org.clojure/clojurescript "0.0-3211"]
+   [prismatic/schema "0.4.2"]]
 
-  :profiles {:repl {:source-paths ["dev"]
-                    :dependencies [[org.clojure/tools.namespace "0.2.10"]]
-                    :jvm-opts ["-DBODY_LOG_APPENDER=repl"]}
-
-             :test {:jvm-opts ["-DBODY_LOG_APPENDER=nop"]}})
+  :cljsbuild
+  {:builds []})
