45a500a902c62bfc0452fc0d69f31f5962d7d24b
Greg Look <greg@greg-look.net>
2015-05-05 18:14:33 -0700

    Remove old code.

--- src/mvxcvi/body/web/helpers.clj
+++
@@ -1,7 +0,0 @@
-(ns counsyl.lambda-ci.web.helpers
-  "Functions for generating URLs and other common pieces of markup."
-  (:require
-    [clojure.string :as str]))
-
-
-; ...
--- test/mvxcvi/body/core_test.clj
+++
@@ -1,8 +0,0 @@
-(ns mvxcvi.body.core-test
-  (:require
-    [clojure.test :refer :all]))
-
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
